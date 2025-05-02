1b1cb4934c5d21a193aa2579c3f3eb99f95e1021
Viduni-Hewage <viduhewage02@gmail.com>
2025-05-02 12:31:49 +0530

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+export const test = (req, res) => {
+    res.json({
+        message: 'API is working!', 
+    }); 
+}
+
+// req=> is what the client sends to the server/browser
+// res => is what the server sends to the client/browser
