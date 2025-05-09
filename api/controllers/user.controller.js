a379fc19c88e6c55029ce3b9a752e39d55991fd6
Viduni-Hewage <viduhewage02@gmail.com>
2025-05-09 17:20:35 +0530

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
 export const test = (req, res) => {
     res.json({
         message: 'API is working!', 
@@ -6,3 +10,27 @@ export const test = (req, res) => {
 
 // req=> is what the client sends to the server/browser
 // res => is what the server sends to the client/browser
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account!'));
+    try {
+       if(req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+            }
+        }, { new: true });
+
+        const { password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest);
+
+            
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,18 @@
 import { useSelector} from 'react-redux'
-import React from 'react'
+import {useRef} from 'react'
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const {currentUser} = useSelector((state) => state.user);
   return (
     <div className='p-3 max-w-lg mx-auto'>
      <h1 className = 'text-3xl font-semibold text-center my-7' >Profile</h1>
+
      <form className='flex flex-col gap-4'>
-        <img src = {currentUser.photo} alt = "profile"
-        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/> 
+      <input type = "file" ref = {fileRef} hidden accept = 'image/*'/>
+        <img onClick = {()=>fileRef.current.click()} src = {currentUser.photo} alt = "profile"
+        className='rounded-full h-24 w-24 object-cover 
+        cursor-pointer self-center mt-2'/> 
         <input type = 'text' placeholder='username' id = 'username'
         className='border p-3 rounded-lg'/> 
         <input type = 'text' placeholder='email' id = 'email'
@@ -17,10 +21,12 @@ export default function Profile() {
         className='border p-3 rounded-lg'/>   
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Update</button>   
       </form>
+
       <div className = 'flex justify-between mt-5'>
         <span className = 'text-red-700 cursor-pointer'>Delete Account</span>
         <span className = 'text-red-700 cursor-pointer'>Sign out</span>
       </div>
+
     </div>
   )
 }
